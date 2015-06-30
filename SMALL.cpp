5ce63ede003f15b2fc72dd35f291fce92ef5b982
Krishna Kalubandi <krishnakalubandi@gmail.com>
2015-06-30 11:04:19 +0530

    reduced running time with precomputation

--- SMALL.cpp
+++ SMALL.cpp
@@ -1,7 +1,7 @@
 /* 
 * @Author: Krishna Kalubandi
 * @Date:   2015-06-29 14:56:19
-* @Last Modified time: 2015-06-29 17:26:51
+* @Last Modified time: 2015-06-30 10:00:20
 */
 
 
@@ -19,8 +19,9 @@ using namespace std;
 int max (int a, int b) { return a > b ? a : b;}
 int gcd(long  a, long  b){ return (b==0)? a : gcd(b, a%b);}
 const int seive = 10010;
-int isprime[seive];
+bool isprime[seive];
 int sq[seive];
+int table[seive+1];
 long p = 1000000007;
 void calcPrimes(){
 		for(int i = 2; i * i < seive; i++){
@@ -29,36 +30,45 @@ void calcPrimes(){
 				{
 					isprime[j] = true;
 				}
-			}
-		}
-		for (int i = 2; i < seive; ++i)
-		{
-			if(!isprime[i])
-			{
 				for (int j = i; j<seive ; j *= i ) {
                     sq[j] = i;
                 }
 			}
 		}
 	}
-long lcm(int n)
+/*long lcm(int n)
 {
 	 	if(n == 1)
             return 1;
         else if(sq[n] != 0)
             return (lcm(n-1) * sq[n]) % p;
+        else if(n * n > 10000 && !isprime[n])
+        	return (lcm(n-1) * n) % p;
         else
             return lcm(n-1);
+}*/
+void precompute(){
+	table[1] = 1;
+	for (int i = 2; i <= seive; ++i)
+	{
+		if(sq[i] != 0) 
+            table[i] = ((long)table[i-1] * sq[i]) % p;
+        else if(i * i > 10000 && !isprime[i]) 
+        	table[i] = ((long)table[i-1] * i) % p;
+        else
+        	table[i] = table[i-1];
+	}
 }
 int main(){
 	calcPrimes();
+	precompute();
 	int t;
 	cin>>t;
 	while(t--)
 	{
 		int n;
 		scanf("%d",&n);
-		printf("%lld\n",lcm(n));
+		printf("%d\n",table[n]);
 	}
     return 0;
 }
